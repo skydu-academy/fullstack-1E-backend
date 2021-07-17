5d460533df827df205af30a8a5acbc1444de4e35
ant094 <cahyono.ari80@gmail.com>
2021-07-17 20:11:14 +0700

    Add hasFactory user

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use App\Http\Requests\Auth\LoginPostRequest;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\Auth;
